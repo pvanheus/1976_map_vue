573b2bb006a295910f33cc93cee2123281afd82c
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-13 22:52:18 +0200

    * embed result of site_data function

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="col s8 offset-s2">
       <Header />
       <Map />
+      <pre v-if="site_data">{{site_data}}</pre>
     </div>
   </div>
 </template>
@@ -10,12 +11,24 @@
 <script>
 import Header from "./components/Header";
 import Map from "./components/Map";
+import {AxiosInstance as axios} from "axios";
 
 export default {
   name: 'MainPage',
   components: {
     Header,
     Map
+  },
+  data: () => {
+    return {
+      site_data: null
+    }
+  },
+  mounted: function () {
+    let url = 'http://1976.webbedfeet.co.za/.netlify/functions/site_data';
+    axios.get(url).then(response => {
+      this.site_data = response.data
+    });
   }
 };
 </script>
