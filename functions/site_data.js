436c1e54605cf2113ff73dc0adf76a19349480b3
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-13 23:12:01 +0200

    * only respond with key if referrer is ok in site_data.js

--- functions/site_data.js
+++ functions/site_data.js
@@ -1,8 +1,10 @@
 exports.handler = (event, context, callback) => {
-    let headers = event.headers;
-    headers.KEY = process.env.TF_KEY;
+    let response = {};
+    if ('referer' in event.headers && event.headers.referer === 'https://1976.webbedfeet.co.za/') {
+        response.TF_KEY = process.env.TF_KEY
+    }
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify(headers),
+        body: JSON.stringify(response),
     })
 };
