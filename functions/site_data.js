c97662a6a40d2b249e712817dbb4849469eb387b
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-13 22:12:07 +0200

    * Update site_data function

--- functions/site_data.js
+++ functions/site_data.js
@@ -1,6 +1,8 @@
 exports.handler = (event, context, callback) => {
+    let headers = event.headers;
+    headers.KEY = process.env.TF_KEY;
     callback(null, {
         statusCode: 200,
-        body: 'No worries, all is working fine!'
+        body: JSON.stringify(headers),
     })
 };
