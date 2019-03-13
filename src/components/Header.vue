7474c620eeb813abc5794a774a1891871c9a5f28
Peter van Heusden <pvh@sanbi.ac.za>
2019-03-14 00:59:04 +0200

    * center header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col s8 offset-s2">
         <div id="header">
-          <h4>In memory of 1976</h4>
+          <h4 class="center-align">In memory of 1976</h4>
           <p>
             June 16 1976 marks the start of the 1976 youth uprising against
             Apartheid in South Africa. Most closely associated with the school
