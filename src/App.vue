c650380568d3b460f91706aefdd827c212525667
Simo Mafuxwana <simodms@gmail.com>
2019-06-28 14:42:49 +0200

    Ag Grid CSS imports

--- src/App.vue
+++ src/App.vue
@@ -110,4 +110,7 @@
   };
 </script>
 
-<style></style>
+<style lang="scss">
+  @import "../node_modules/ag-grid-community/dist/styles/ag-grid.css";
+  @import "../node_modules/ag-grid-community/dist/styles/ag-theme-balham.css";
+</style>
