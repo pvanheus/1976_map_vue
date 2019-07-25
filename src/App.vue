1b885c2f30660373e2c7af49d17660500cdc40e1
Simo Mafuxwana <simodms@gmail.com>
2019-07-25 21:10:38 +0200

    boom

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <Nav />
+    <br />
+
+    <div class="uk-grid-small uk-child-width-expand@s" uk-grid>
+      <div>
+        <button class="uk-button uk-button-default uk-button-large">Filter</button>
+        <button class="uk-button uk-button-default uk-button-large">Filter</button>
+        <button class="uk-button uk-button-default uk-button-large">Filter</button>
+        <button class="uk-button uk-button-default uk-button-large">Filter</button>
+      </div>
+      <div>
+        <Slider @updateEnd="updateEnd" />
+      </div>
+    </div>
+
     <div class="uk-grid-small uk-child-width-expand@s" uk-grid>
       <Map v-bind:start-timestamp="startTimeStamp" v-bind:end-timestamp="endTimeStamp" />
-      <div class="uk-width-1-4">
-        <Slider @updateEnd="updateEnd" />
-        <Header1 />
+      <div class="uk-width-1-2">
+        <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+          <div v-for="(item, index) in deaths.slice(1, 7)" :key="index">
+            <div class="uk-card uk-card-default uk-card-body">
+              <h4 class="uk-card-title">{{ item.person }}</h4>
+              <p>{{ item.detail }}</p>
+              <p>{{ item.place }}</p>
+              <p>{{ item.sex }}</p>
+              <p>{{ item.age }}</p>
+            </div>
+          </div>
+        </div>
+        <!-- <Header1 /> -->
       </div>
     </div>
     <div id="row2Wrapper" class="uk-grid-small uk-child-width-expand@s" uk-grid>
